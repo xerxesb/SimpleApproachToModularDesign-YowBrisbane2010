8d76264ce1bc34b58534c48b68a74b1753ec9408
J. B. Rainsberger <me@jbrains.ca>
2010-12-08 13:53:17 +1000

    Moved pricesByBarcode out into the client. Extracted a few methods to clarify intent.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -9,36 +9,49 @@ import org.junit.*;
 
 public class SellOneItemTest {
     public static class Sale {
-        private final StringWriter canvas;
+        private StringWriter canvas;
+        private Map<String, String> pricesByBarcode;
 
-        public Sale(StringWriter canvas) {
+        public Sale(StringWriter canvas, Map<String, String> pricesByBarcode) {
             this.canvas = canvas;
+            this.pricesByBarcode = pricesByBarcode;
         }
 
         public void onBarcode(String barcode) {
+            // SMELL Seems like I should move this out a new
+            // client that validates the barcodes. Not sure.
             if ("".equals(barcode)) {
                 canvas.write("Scanning error: empty barcode");
                 return;
             }
 
-            Map<String, String> pricesByBarcode = new HashMap<String, String>() {
-                {
-                    put("123", "$9.50");
-                    put("456", "$17.26");
-                }
-            };
-
             if (pricesByBarcode.containsKey(barcode))
-                canvas.write(pricesByBarcode.get(barcode));
+                displayPrice(findPriceByBarcode(barcode));
             else
-                canvas.write("No product found for " + barcode);
+                displayProductNotFoundMessage(barcode);
+        }
+
+        private void displayPrice(String price) {
+            canvas.write(price);
+        }
+
+        private String findPriceByBarcode(String barcode) {
+            return pricesByBarcode.get(barcode);
+        }
+
+        private void displayProductNotFoundMessage(String barcode) {
+            canvas.write("No product found for " + barcode);
         }
     }
 
     @Test
     public void productFound() throws Exception {
         StringWriter canvas = new StringWriter();
-        Sale sale = new Sale(canvas);
+        Sale sale = new Sale(canvas, new HashMap<String, String>() {
+            {
+                put("123", "$9.50");
+            }
+        });
 
         sale.onBarcode("123");
 
@@ -48,7 +61,11 @@ public class SellOneItemTest {
     @Test
     public void anotherProductFound() throws Exception {
         StringWriter canvas = new StringWriter();
-        Sale sale = new Sale(canvas);
+        Sale sale = new Sale(canvas, new HashMap<String, String>() {
+            {
+                put("456", "$17.26");
+            }
+        });
 
         sale.onBarcode("456");
 
@@ -58,7 +75,7 @@ public class SellOneItemTest {
     @Test
     public void productNotFound() throws Exception {
         StringWriter canvas = new StringWriter();
-        Sale sale = new Sale(canvas);
+        Sale sale = new Sale(canvas, Collections.<String, String> emptyMap());
 
         sale.onBarcode("999");
 
@@ -68,7 +85,7 @@ public class SellOneItemTest {
     @Test
     public void emptyBarcode() throws Exception {
         StringWriter canvas = new StringWriter();
-        Sale sale = new Sale(canvas);
+        Sale sale = new Sale(canvas, null);
 
         sale.onBarcode("");
 
